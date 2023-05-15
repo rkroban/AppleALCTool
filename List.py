3e011030f55c4e754b058a373ba05e10347f3596
rkroban <robankarthik@gmail.com>
2023-05-15 16:55:44 -0500

    Update List.py

--- List.py
+++ List.py
@@ -1,6 +1,6 @@
 with open("codecList.txt") as list:
     codecList = list.read()
-    if '0257' in codecList:
+    if 'bababoey' in codecList:
         print('string exists in file')
     else:
         print('your audio codec is currently not supported')

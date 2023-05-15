6bfd6bd0eca097523b8afeac7b8d2055c99da6cb
rkroban <robankarthik@gmail.com>
2023-05-15 17:33:41 -0500

    add file

--- Scan.py
+++ Scan.py
@@ -1,9 +1,1 @@
 import List
-try:
-    output = subprocess.Popen(["devcon","status","*"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)    #useing comma "," not space " " in Popen[]. replace the key word "*" to what you want to search.
-    stdout, stderr = output.communicate()
-    print ("output: \n", output)
-    print ("stdout: \n", stdout)  # output here if ok. 
-    print ("stderr: \n", stderr)  # output if no arg     
-except subprocess.CalledProcessError:
-    print('Exception handled')   

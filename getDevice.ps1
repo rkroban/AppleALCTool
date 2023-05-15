a15a79e085d4db67c3dc382ce976b536cab00f8a
rkroban <robankarthik@gmail.com>
2023-05-15 18:25:48 -0500

    Complete getDevice powershell

--- getDevice.ps1
+++ getDevice.ps1
@@ -1,3 +1,5 @@
 cd
-Import-Module .\PSCmdlet.psd1 –Verbose
+Unblock-File -Path .\DeviceManagementCmdlet.dll
+Unblock-File -Path .\DeviceManagementEngine.dll
+Import-module .\DeviceManagement.psd1
 Get-Device | Sort-Object -Property Name | ft Name, DriverVersion, DriverProvider, IsPresent, HasProblem -AutoSize | Out-File -Append -Force "FileListName.txt"

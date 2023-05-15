92a3cad1ebaef94d3d956656b32f17c47ed7d532
rkroban <robankarthik@gmail.com>
2023-05-15 18:29:14 -0500

    change name of output file

--- getDevice.ps1
+++ getDevice.ps1
@@ -2,4 +2,4 @@ cd
 Unblock-File -Path .\DeviceManagementCmdlet.dll
 Unblock-File -Path .\DeviceManagementEngine.dll
 Import-module .\DeviceManagement.psd1
-Get-Device | Sort-Object -Property Name | ft Name, DriverVersion, DriverProvider, IsPresent, HasProblem -AutoSize | Out-File -Append -Force "FileListName.txt"
+Get-Device | Sort-Object -Property Name | ft Name, DriverVersion, DriverProvider, IsPresent, HasProblem -AutoSize | Out-File -Append -Force "deviceInfo.txt"

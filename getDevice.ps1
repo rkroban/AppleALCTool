9285ab012a3df6d4e31c92488918464dc7c24527
rkroban <robankarthik@gmail.com>
2023-05-15 18:36:17 -0500

    organize files

--- getDevice.ps1
+++ getDevice.ps1
@@ -1,5 +1,5 @@
 cd
-Unblock-File -Path .\DeviceManagementCmdlet.dll
-Unblock-File -Path .\DeviceManagementEngine.dll
-Import-module .\DeviceManagement.psd1
+Unblock-File -Path .\DeviceManagement\DeviceManagementCmdlet.dll
+Unblock-File -Path .\DeviceManagement\DeviceManagementEngine.dll
+Import-module .\DeviceManagement\DeviceManagement.psd1
 Get-Device | Sort-Object -Property Name | ft Name, DriverVersion, DriverProvider, IsPresent, HasProblem -AutoSize | Out-File -Append -Force "deviceInfo.txt"

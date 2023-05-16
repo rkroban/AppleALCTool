c1bdb5300f5184ac1322737b961f4b8e142e8ba0
rkroban <robankarthik@gmail.com>
2023-05-15 19:03:42 -0500

    remove useless code

--- getDevice.ps1
+++ getDevice.ps1
@@ -1,5 +1,1 @@
 cd
-Unblock-File -Path .\DeviceManagement\DeviceManagementCmdlet.dll
-Unblock-File -Path .\DeviceManagement\DeviceManagementEngine.dll
-Import-module .\DeviceManagement\DeviceManagement.psd1
-Get-Device | Sort-Object -Property Name | ft Name, DriverVersion, DriverProvider, -AutoSize | Out-File -Append -Force "deviceInfo.txt"

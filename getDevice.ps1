2793d89cd5847cd8e0e1d0e608a736883ad7b879
rkroban <robankarthik@gmail.com>
2023-05-15 22:28:05 -0500

    convert codeclist to UTF-8

--- getDevice.ps1
+++ getDevice.ps1
@@ -1,3 +1,4 @@
 cd
 .\devcon hwids *HDAUDIO* > deviceInfoBase.txt
+Get-Content .\codecListBase.txt | Set-Content -Encoding UTF8 .\codecList.txt
 Get-Content .\deviceInfoBase.txt | Set-Content -Encoding UTF8 .\deviceInfo.txt

3deda6456ad08eae8e6d5269e1fcf624d0242d95
rkroban <robankarthik@gmail.com>
2023-05-15 22:18:41 -0500

    find line with realtek

--- Main.py
+++ Main.py
@@ -1,16 +1,25 @@
+#by @rkroban
 import List
 import subprocess
+#execute the powershell script with no restrictions until the script is complete
 subprocess.call('powershell.exe -ExecutionPolicy RemoteSigned -file "getDevice.ps1"', shell=True)
+#find the number of lines with text in the output file
 with open("deviceInfo.txt") as devices:
     count = 0
     for line in devices:
         if line != "\n":
             count += 1
 print('Total Lines', count)
+#find the line where the Realtek Hardware ids start in the output file and record it as 'realtk'
 with open("deviceInfo.txt") as devices:
-    realtk = 0
+    realtktmp = 0
     for line in devices:
-        if line == "Realtek":
-            print('Realtek Hardware IDs found at line', realtk)
+        realtk_index = line.find('Realtek')
+        if realtk_index < 0:
+            realtktmp += 1
         else:
-            realtk += 1
+            realtktmp += 1
+            realtk = realtktmp
+            print(f'Realtek hardware ids starting at index {realtk_index} of line {realtk}')
+#delete every line before realtk as it is unnecessary
+print('now deleting unncessesary data to clean up output file')

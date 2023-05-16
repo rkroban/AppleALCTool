6259757fd38a5d05f718ac00a521becb9f6f5acb
rkroban <robankarthik@gmail.com>
2023-05-16 09:24:18 -0500

    delete scan.py as it is unnecesary

--- Scan.py
+++
@@ -1,8 +0,0 @@
-import Main
-with open("codecList.txt") as list:
-    codecList = list.read()
-    if '0257' in codecList:
-        print('string exists in file')
-    else:
-        print('your audio codec is currently not supported')
-        print('=(')

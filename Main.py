cd8277b2a231b667742268cae97f3f86c31fa999
rkroban <robankarthik@gmail.com>
2023-05-15 20:58:57 -0500

    fix gitignore and convert output to UTF8

--- Main.py
+++ Main.py
@@ -1,3 +1,16 @@
 import List
 import subprocess
-power = subprocess.call('powershell.exe -ExecutionPolicy RemoteSigned -file "getDevice.ps1"', shell=True)
+subprocess.call('powershell.exe -ExecutionPolicy RemoteSigned -file "getDevice.ps1"', shell=True)
+with open("deviceInfo.txt") as devices:
+    count = 0
+    for line in devices:
+        if line != "\n":
+            count += 1
+print('Total Lines', count)
+with open("deviceInfo.txt") as devices:
+    realtk = 0
+    for line in devices:
+        if line == "Realtek":
+            print('Realtek Hardware IDs found at line', realtk)
+        else:
+            realtk += 1

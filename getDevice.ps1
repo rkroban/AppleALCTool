756c82d4e1b60da40de1b3b07c4e8be91d8d294f
rkroban <robankarthik@gmail.com>
2023-05-16 18:54:56 -0500

    add intel support

--- getDevice.ps1
+++ getDevice.ps1
@@ -1,4 +1,4 @@
 cd
-.\devcon hwids *HDAUDIO* > deviceInfoBase.txt
+.\devcon hwids *HDAUDIO* *INTELAUDIO* > deviceInfoBase.txt
 Get-Content .\codecListBase.txt | Set-Content -Encoding UTF8 .\codecList.txt
 Get-Content .\deviceInfoBase.txt | Set-Content -Encoding UTF8 .\deviceInfo.txt
